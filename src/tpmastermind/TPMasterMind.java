202f511c850bd17b47af35e8fa6d8185a53a690b
bigwanjeog <wenaud@live.fr>
2016-09-01 15:19:34 +0200

    Changement de méthode, utilisation de tableau a 2 dimensions

--- src/tpmastermind/TPMasterMind.java
+++ src/tpmastermind/TPMasterMind.java
@@ -5,30 +5,33 @@ package tpmastermind;
  */
 public class TPMasterMind {
 
-    static int tSoluce[] = {4, 5, 1, 2};//solution test
-    static int tProp[] = {5, 4, 1, 2};//proposition test
+    static int[][] tMasterMind = {{3,2,2,1},{3,2,2,1}};
+    static int reponseOk = 1000;
 
     public static void main(String[] args) {
         //TODO plusieurs fois la meme couleur
-
         System.out.println(verification());
-
     }
+
+
+
 
     public static String verification() {
         int cptMalPlacer = 0;
         int cptBienPlacer = 0;
-        for (int i = 0; i < tProp.length; i++){
-            for(int j = 0; j < tSoluce.length; j++){
-                if (tProp[i] == tSoluce[j]){
-                    if (tProp[i] == tSoluce[i]){
-                        cptBienPlacer++;
-                    } else {
-                        cptMalPlacer++;
-                    }
-                }
+        for (int i = 0; i < tMasterMind[0].length; i++){
+            if(tMasterMind[0][i] == tMasterMind[1][i]){
+                cptBienPlacer++;
+                tMasterMind[0][i] = reponseOk();
+                tMasterMind[1][i] = reponseOk();
             }
         }
-        return "Solution bien placé " + cptBienPlacer + "\nSolution mal placé " + cptMalPlacer;
+
+        return "Solution bien placé : " + cptBienPlacer + "\nSolution mal placé : " + cptMalPlacer;
+    }
+
+    public static int reponseOk() {
+        reponseOk++;
+        return reponseOk;
     }
 }
